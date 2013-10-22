ce4e1ee807890419fceaf267a48c08896dba5da9
Rui Cheng <ktei2008@gmail.com>
2013-10-23 00:01:01 +1100

    adds cheating is wrong mechanism

--- src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.geoquiz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -11,90 +12,54 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends Activity {
-	
+
 	private static final String TAG = "QuizActivity";
-	
+
 	private Button mTrueButton;
-    private Button mFalseButton;
-    private Button mNextButton;
-    private Button mCheatButton;
-    private TextView mQuestionTextView;
+	private Button mFalseButton;
+	private Button mNextButton;
+	private Button mCheatButton;
+	private TextView mQuestionTextView;
 
-    private TrueFalse[] mQuestionBank = new TrueFalse[] {
-        new TrueFalse(R.string.question_oceans, true),
-        new TrueFalse(R.string.question_mideast, false),
-        new TrueFalse(R.string.question_africa, false),
-        new TrueFalse(R.string.question_americas, true),
-        new TrueFalse(R.string.question_asia, true),
-    };
-    
-    private int mCurrentIndex = 0;
+	private TrueFalse[] mQuestionBank = new TrueFalse[] {
+			new TrueFalse(R.string.question_oceans, true),
+			new TrueFalse(R.string.question_mideast, false),
+			new TrueFalse(R.string.question_africa, false),
+			new TrueFalse(R.string.question_americas, true),
+			new TrueFalse(R.string.question_asia, true), };
+
+	private int mCurrentIndex = 0;
+	private boolean mIsCheater;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		Log.d(TAG, "onCreate(Bundle) called");
-		setContentView(R.layout.activity_quiz);
-		
-        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
-		
-		mTrueButton = (Button)findViewById(R.id.true_button);
-		mTrueButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkAnswer(true);
-            }
-        });
-		
-        mFalseButton = (Button)findViewById(R.id.false_button);
-        mFalseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            	checkAnswer(false);
-            }
-        });
-        
-        mNextButton = (Button)findViewById(R.id.next_button);
-        mNextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                updateQuestion();
-            }
-        });
-        
-        updateQuestion();
+	public void onStart() {
+		super.onStart();
+		Log.d(TAG, "onStart() called");
 	}
-	
+
 	@Override
-    public void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart() called");
-    }
+	public void onPause() {
+		super.onPause();
+		Log.d(TAG, "onPause() called");
+	}
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause() called");
-    }
-    
-    @Override
-    public void onResume() {
-    	super.onResume();
-        Log.d(TAG, "onResume() called");
-    }
-    
-    @Override
-    public void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop() called");
-    }
+	@Override
+	public void onResume() {
+		super.onResume();
+		Log.d(TAG, "onResume() called");
+	}
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy() called");
-    }
+	@Override
+	public void onStop() {
+		super.onStop();
+		Log.d(TAG, "onStop() called");
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Log.d(TAG, "onDestroy() called");
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -102,23 +67,85 @@ public class QuizActivity extends Activity {
 		getMenuInflater().inflate(R.menu.quiz, menu);
 		return true;
 	}
-	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Log.d(TAG, "onCreate(Bundle) called");
+		setContentView(R.layout.activity_quiz);
+
+		mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+
+		mTrueButton = (Button) findViewById(R.id.true_button);
+		mTrueButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				checkAnswer(true);
+			}
+		});
+
+		mFalseButton = (Button) findViewById(R.id.false_button);
+		mFalseButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				checkAnswer(false);
+			}
+		});
+
+		mNextButton = (Button) findViewById(R.id.next_button);
+		mNextButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				mIsCheater = false;
+				updateQuestion();
+			}
+		});
+
+		mCheatButton = (Button) findViewById(R.id.cheat_button);
+		mCheatButton.setOnClickListener(new View.OnClickListener() {
+			// @Override
+			public void onClick(View v) {
+				Intent i = new Intent(QuizActivity.this, CheatActivity.class);
+				boolean answerIsTrue = mQuestionBank[mCurrentIndex]
+						.isTrueQuestion();
+				i.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+				startActivityForResult(i, 0);
+			}
+		});
+
+		updateQuestion();
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (data == null) {
+			return;
+		}
+		mIsCheater = data.getBooleanExtra(CheatActivity.EXTRA_ANSWER_SHOWN,
+				false);
+	}
+
 	private void updateQuestion() {
 		int question = mQuestionBank[mCurrentIndex].getQuestion();
-        mQuestionTextView.setText(question);
+		mQuestionTextView.setText(question);
 	}
-	
+
 	private void checkAnswer(boolean userPressedTrue) {
 		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
 		int messageResId = 0;
 
-        if (userPressedTrue == answerIsTrue) {
-            messageResId = R.string.correct_toast;
-        } else {
-            messageResId = R.string.incorrect_toast;
-        }
+		if (mIsCheater) {
+			messageResId = R.string.judgment_toast;
+		} else {
+			if (userPressedTrue == answerIsTrue) {
+				messageResId = R.string.correct_toast;
+			} else {
+				messageResId = R.string.incorrect_toast;
+			}
+		}
 
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+		Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
 	}
 
 }
